4b30db06ce7a5f92b40e4ff0dcd85310877169a8
projetoerp <projetoerp3gmail.com>
2016-09-09 00:19:25 -0300

    atualizacao

--- Project1.dpr
+++ Project1.dpr
@@ -5,15 +5,15 @@ uses
   Unit1 in 'Unit1.pas' {fGridBanco},
   Unit2 in 'Unit2.pas' {fCadBanco},
   udm in 'udm.pas' {dmodule: TDataModule},
-  UAssistente_Filtro in 'C:\ERP\fontes\padrao\forms\UAssistente_Filtro.pas' {FrAssistenteFiltro},
-  uCadastros in 'C:\ERP\fontes\padrao\forms\uCadastros.pas' {frmCadastros},
-  uGrids in 'C:\ERP\fontes\padrao\forms\uGrids.pas' {frmGrids},
-  UMensagem in 'C:\ERP\fontes\padrao\forms\UMensagem.pas' {FrMensagem},
-  UMenu in 'C:\ERP\fontes\padrao\forms\UMenu.pas' {FrMenu},
-  uBanco in 'C:\ERP\fontes\padrao\units\uBanco.pas',
-  uConst in 'C:\ERP\fontes\padrao\units\uConst.pas',
-  uFuncoes in 'C:\ERP\fontes\padrao\units\uFuncoes.pas',
-  uValidarCampos in 'C:\ERP\fontes\padrao\forms\uValidarCampos.pas' {FrmValidarCampos};
+  UAssistente_Filtro in 'C:\ERP\projeto-ERP\fontes\padrao\forms\UAssistente_Filtro.pas' {FrAssistenteFiltro},
+  uCadastros in 'C:\ERP\projeto-ERP\fontes\padrao\forms\uCadastros.pas' {frmCadastros},
+  uGrids in 'C:\ERP\projeto-ERP\fontes\padrao\forms\uGrids.pas' {frmGrids},
+  UMensagem in 'C:\ERP\projeto-ERP\fontes\padrao\forms\UMensagem.pas' {FrMensagem},
+  UMenu in 'C:\ERP\projeto-ERP\fontes\padrao\forms\UMenu.pas' {FrMenu},
+  uBanco in 'C:\ERP\projeto-ERP\fontes\padrao\units\uBanco.pas',
+  uConst in 'C:\ERP\projeto-ERP\fontes\padrao\units\uConst.pas',
+  uFuncoes in 'C:\ERP\projeto-ERP\fontes\padrao\units\uFuncoes.pas',
+  uValidarCampos in 'C:\ERP\projeto-ERP\fontes\padrao\forms\uValidarCampos.pas' {FrmValidarCampos};
 
 {$R *.res}
 
